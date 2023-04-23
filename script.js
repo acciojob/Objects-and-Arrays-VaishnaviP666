89b6091bcbdc5053759e5b3362d8f9775776edb4
VaishnaviP666 <122201508+VaishnaviP666@users.noreply.github.com>
2023-04-23 23:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,25 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+var team =players["reference"];
+
+int team1[] = new int[players.length];
+
+for (int i = 0; i < players.length; i++)
+            System.out.print(players[i] + " ");
+  
+
+        for (int i = 0; i < team1.length; i++)
+            System.out.print(team1[i] + " ");
+
+int cap1[] = new int[person.length];
+
+for (int i = 0; i < person.length; i++)
+            System.out.print(person[i] + " ");
+  
+
+        for (int i = 0; i < cap1.length; i++)
+            System.out.print(cap1[i] + " ");
 
 window.players = players;
 window.person = person;
